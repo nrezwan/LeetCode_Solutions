9b0e95176169b4cc1ac81a4dc1ed9c35d2182c0b
nrezwan <122240133+nrezwan@users.noreply.github.com>
2026-08-20 04:29:03 -0300

    Time: 32 ms (21.03%) | Memory: 48.5 MB (96.42%) - LeetSync

---
+++ 1-two-sum/two-sum.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        for(int i=0; i<nums.Length-1; i++){
+            for(int j=i+1; j<nums.Length;j++){
+                if (nums[i] + nums[j] == target)
+                    return [i, j];
+            }
+        }
+        return res;
+    }
+}
