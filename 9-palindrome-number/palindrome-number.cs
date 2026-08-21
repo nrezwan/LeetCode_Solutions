64a87006cb758d14ea15b541b04de84f7256a217
nrezwan <122240133+nrezwan@users.noreply.github.com>
2026-08-21 08:55:35 -0300

    Time: 1 ms (99.94%) | Memory: 36.5 MB (96.77%) - LeetSync

--- 9-palindrome-number/palindrome-number.cs
+++ 9-palindrome-number/palindrome-number.cs
@@ -1,67 +1,17 @@
 public class Solution {
-    // ListNode class for our linked list
-    public class ListNode {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null) {
-            this.val = val;
-            this.next = next;
-        }
-    }
-    
     public bool IsPalindrome(int x) {
-        // Negative numbers are not palindromes
-        if (x < 0) return false;
-        
-        // Convert number to digits
-        List<int> digits = new List<int>();
-        int temp = x;
-        while (temp > 0) {
-            digits.Add(temp % 10);
-            temp /= 10;
-        }
-        // Handle x = 0
-        if (digits.Count == 0) {
-            digits.Add(0);
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
         }
         
-        // Create forward linked list
-        ListNode forwardHead = CreateLinkedList(digits);
+        int reversedHalf = 0;
         
-        // Create backward linked list
-        List<int> reversedDigits = new List<int>(digits);
-        reversedDigits.Reverse();
-        ListNode backwardHead = CreateLinkedList(reversedDigits);
-        
-        // Compare the two linked lists
-        return CompareLinkedLists(forwardHead, backwardHead);
-    }
-    
-    private ListNode CreateLinkedList(List<int> digits) {
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
-        
-        foreach (int digit in digits) {
-            current.next = new ListNode(digit);
-            current = current.next;
+        while (x > reversedHalf) {
+            int digit = x % 10;
+            reversedHalf = reversedHalf * 10 + digit;
+            x /= 10;
         }
-        
-        return dummy.next;
-    }
-    
-    private bool CompareLinkedLists(ListNode list1, ListNode list2) {
-        ListNode current1 = list1;
-        ListNode current2 = list2;
-        
-        while (current1 != null && current2 != null) {
-            if (current1.val != current2.val) {
-                return false;
-            }
-            current1 = current1.next;
-            current2 = current2.next;
-        }
-        
-        // Both lists should be same length
-        return current1 == null && current2 == null;
+
+        return x == reversedHalf || x == reversedHalf / 10;
     }
 }
