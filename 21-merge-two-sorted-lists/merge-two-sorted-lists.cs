012fe0167ecf8aba5f961482e0280597fe707385
nrezwan <122240133+nrezwan@users.noreply.github.com>
2026-08-22 21:11:18 -0300

    Time: 0 ms (100.00%) | Memory: 43 MB (76.60%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cs
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cs
@@ -11,16 +11,31 @@
  */
 public class Solution {
     public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1 == null) return list2;
-        if(list2 == null) return list1;
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
 
-        if(list1.val < list2.val){
-            list1.next = MergeTwoLists(list1.next,list2);
-            return list1;
+        if (list1 != null) {
+            current.next = list1;
+        } 
+        else if(list2 != null){
+            current.next = list2;
         }
         else{
-            list2.next = MergeTwoLists(list1,list2.next);
-            return list2;
+            current.next = null;
         }
+
+        
+        return dummy.next;
     }
 }
